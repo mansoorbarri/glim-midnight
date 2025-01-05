f6a80a98dac4752709da0fa1daa4e4931f9a0c5a
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2025-01-05 11:51:28 +0000

    remove .gitmodules

---
+++ examplesite/public/js/script.js
@@ -0,0 +1,37 @@
+// typewritter
+let typeJsText = document.querySelector(".animatedText");
+let stringIndex = 0;
+let charIndex = 0;
+let isTyping = true;
+
+function typeJs() {
+    if (stringIndex < textArray.length) {
+        const currentString = textArray[stringIndex];
+
+        if (isTyping) {
+            if (charIndex < currentString.length) {
+                typeJsText.innerHTML += currentString.charAt(charIndex);
+                charIndex++;
+            } else {
+                isTyping = false;
+            }
+        } else {
+            if (charIndex > 0) {
+                typeJsText.innerHTML = currentString.substring(0, charIndex - 1);
+                charIndex--;
+            } else {
+                isTyping = true;
+                stringIndex++;
+
+                if (stringIndex >= textArray.length) {
+                    stringIndex = 0;
+                }
+
+                charIndex = 0;
+                typeJsText.innerHTML = "";
+            }
+        }
+    }
+}
+
+setInterval(typeJs, 120);
